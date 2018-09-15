174753f98aa0103549aa65445bac06fae183d229
lcfelipe <lcfelipe@outlook.com>
2018-09-15 16:34:47 -0300

    Ajuste no endpoint

--- src/main/java/com/felipe/cursomc/services/CategoriaService.java
+++ src/main/java/com/felipe/cursomc/services/CategoriaService.java
@@ -7,6 +7,7 @@ import java.util.Optional;
 
 import com.felipe.cursomc.domain.Categoria;
 import com.felipe.cursomc.repositories.CategoriaRepository;
+import com.felipe.cursomc.services.exception.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -16,6 +17,7 @@ public class CategoriaService {
 	
 	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
-	}
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+		"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+		}
 }
